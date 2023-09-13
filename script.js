4fb92e98829c583b1528c084bc16b2c774cb474b
RickySmith2006 <141283534+RickySmith2006@users.noreply.github.com>
2023-09-14 10:57:01 +1200

    14/09

--- script.js
+++ script.js
@@ -1,15 +1,34 @@
 const cursor = document.querySelector('.cursor');
 const holes = [...document.querySelectorAll('.hole')]
+const scoreEl = document.querySelector('.score span')
+let score = 0
 
 function run(){
     const i = Math.floor(Math.random() * holes.length)
     const hole = holes[i]
+    let timer = null
 
     const img = document.createElement('img')
     img.classList.add('kiwi')
     img.src = 'assets/kiwi.png'
+    
+    img.addEventListener('click', () => {
+        score += 1
+        scoreEl.textContent = score
+        img.src = 'assets/kiwi-whacked.png'
+        clearTimeout(timer)
+        setTimeout(() => {
+            hole.removeChild(img)
+            run()
+        }, 500)
+    })
 
     hole.appendChild(img)
+
+    timer = setTimeout(() => {
+        hole.removeChild(img)
+        run()
+    }, 1000)
 }
 run()
 
