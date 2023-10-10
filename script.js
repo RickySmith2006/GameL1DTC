efe73e18e12c7c7fad30b26208d4afe4a9795729
RickySmith2006 <141283534+RickySmith2006@users.noreply.github.com>
2023-10-11 09:38:14 +1300

    11/10
    chatgbt was used to add the end of the game events

--- script.js
+++ script.js
@@ -4,10 +4,41 @@ const holes = [...document.querySelectorAll('.hole')]
 const scoreEl = document.querySelector('.score span')
 
 // Initialize the score variable
-let score = 0
+let score = 0;
+
+// Function to end the game when reaching 10 points
+function endGame() {
+    if (score >= 10) {
+        // Hide the cursor
+        cursor.style.display = 'none';
+        
+        // Display the "You Won" message
+        const endScreen = document.createElement('div');
+        endScreen.classList.add('end-screen');
+        endScreen.innerHTML = `
+            <div class="end-message">
+                <h2>You Won!</h2>
+                <button class="reset-button">Play Again</button>
+            </div>
+        `;
+        document.body.appendChild(endScreen);
+
+        // Add a click event listener to the reset button
+        const resetButton = document.querySelector('.reset-button');
+        resetButton.addEventListener('click', () => {
+            // Reload the page to restart the game
+            location.reload();
+        });
+    }
+}
 
 // Function to run the game
 function run() {
+    // Check if the game has ended (score is 10)
+    if (score === 10) {
+        return; // Do nothing if the game has ended
+    }
+
     // Generate a random index to select a hole
     const i = Math.floor(Math.random() * holes.length)
     const hole = holes[i]
@@ -19,9 +50,7 @@ function run() {
     img.src = 'assets/kiwi.png'
     
     // Prevent the image from being dragged
-    img.ondragstart = () => {
-        return false
-    }
+    img.draggable = false;
     
     // Add a click event listener for the kiwi
     img.addEventListener('click', () => {
@@ -29,12 +58,18 @@ function run() {
         score += 1
         scoreEl.textContent = score
         
+        // Check if the game has ended
+        endGame();
+        
         // Change the image source and handle kiwi removal
         img.src = 'assets/kiwi-whacked.png'
         clearTimeout(timer)
         setTimeout(() => {
             hole.removeChild(img)
-            run()
+            // Continue the game if the score is less than 10
+            if (score < 10) {
+                run();
+            }
         }, 50)
     })
 
@@ -44,8 +79,11 @@ function run() {
     // Set a timer to remove the kiwi after 500 milliseconds
     timer = setTimeout(() => {
         hole.removeChild(img)
-        run()
-    }, 700)
+        // Continue the game if the score is less than 10
+        if (score < 10) {
+            run();
+        }
+    }, 800)
 }
 
 // Start the game
@@ -66,3 +104,5 @@ window.addEventListener('mousedown', () => {
 window.addEventListener('mouseup', () => {
     cursor.classList.remove('active')
 })
+
+// chatgbt was used to add the end of the game events
